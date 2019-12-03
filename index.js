8a48d17de133def4b3711716d4e53cb835628ded
Phạm Đức <minhduc.096.99@gmail.com>
2019-12-03 21:27:17 +0700

    .

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const topSongsUrl = rootUrl + '/topsongs/all'
 const topAlbumsUrl = rootUrl + '/topalbums/all/limit=20/json'
 
 
-async function getData (quantity) {
+async function getData (feedType, quantity) {
   const songContainer = document.getElementById('songs-content')
   songContainer.innerHTML = ''
-  const request = await fetch(topSongsUrl + `/limit=${quantity}/json`)
-  let data = await request.json()
-  console.log(data)
-  data = data.feed.entry
-  for (let i=0; i<data.length; i++) {
-    const songName = data[i]['im:name'].label
-    const artistName = data[i]['im:artist'].label
-    const image = data[i]['im:image'][0].label
-    const audio = data[i].link[1].attributes.href
-    songContainer.innerHTML += `
+  if (feedType == 'topsong') {
+    const request = await fetch(topSongsUrl + `/limit=${quantity}/json`)
+    let data = await request.json()
+    data = data.feed.entry
+    for (let i = 0; i < data.length; i++) {
+      const songName = data[i]['im:name'].label
+      const artistName = data[i]['im:artist'].label
+      const image = data[i]['im:image'][0].label
+      const audio = data[i].link[1].attributes.href
+      songContainer.innerHTML += `
     <div class="song">
       <img src='${image}'>
       <p>${songName}</p>
@@ -23,27 +23,38 @@ async function getData (quantity) {
       <audio src='${audio}' class='song-play'></audio>
     </div>
     `
-  }
-
-
-  const songElement = document.getElementsByClassName('song')
-  for (let i=0; i<songElement.length; i++) {
-    songElement[i].addEventListener('click', function (event) {
-      for (let j=0; j<songElement.length; j++) {
-        songElement[j].children[3].pause()
-      }
-      const audioElement = event.target.parentNode.children[3]
-      if (audioElement.paused) {
-        audioElement.play()
-      }
-    })
+    }
+    const songElement = document.getElementsByClassName('song')
+    for (let i = 0; i < songElement.length; i++) {
+      songElement[i].addEventListener('click', function (event) {
+        for (let j = 0; j < songElement.length; j++) {
+          songElement[j].children[3].pause()
+        }
+        const audioElement = event.target.parentNode.children[3]
+        if (audioElement.paused) {
+          audioElement.play()
+        }
+      })
+    }
+  } else {
+    const request = await fetch(topAlbumsUrl + `/limit=${quantity}/json`)
+    const data = await request.json();
+    console.log(data)
   }
 
 }
 
 const quantityOption = document.getElementById('quantity')
+const feedOption = document.getElementById('feed-type')
+
 quantityOption.addEventListener('change', function(event){ // change event use for select tag
-  getData(event.target.value);
+  getData(feedOption.value, event.target.value);
 })
+
+feedOption.addEventListener('change', function(event){
+  feedType = event.target.value
+  getData(feedType, quantityOption.value)
+})
+
 
 getData(20)
